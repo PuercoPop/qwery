31089d373536c06f4a8d1546fffc36b7ccaf6ca8
Javier Olaechea <pirata@gmail.com>
2013-04-27 17:48:35 -0500

    First Suite

--- src/qwery.js
+++ src/qwery.js
@@ -1,13 +1,31 @@
-(function (name, context, definition){
-    if (typeof module != 'undefined' && module.exports) module.exports = definition();
-    else if (typeof define == 'function' && define.amd) define(definition);
-    else context[name] = definition();
+(function (name, context, definition) {
+    if (typeof module != 'undefined' && module.exports) {
+        module.exports = definition();
+    } else {
+        if (typeof define == 'function' && define.amd) {
+            define(definition);
+        } else {
+            context[name] = definition();
+        };
+    };
 })('qwery', this, function(){
-    function qwery(selector){
-        return [document.createElement('p'),
-                document.createElement('p'),
-                document.createElement('p'),
-                document.createElement('p')];
+    var qwery = function (selector, context){
+        var _qsa(selector) {
+            try {
+                return this.querySelectorAll(select);
+            } catch(e) {
+                return [];
+            }
+        }
+        if (context instanceof NodeList) {
+            for( var i=0 ; i <= context.length; i++){
+                return context[i].querySelectorAll(selector);
+            };
+        } else if (typeof(context) === "string") {
+            return qwery(selector, qwery(context));
+        } else {
+            return document.querySelectorAll(selector);
+        };
     };
     qwery.configure = function(){};
     qwery.uniq = function(list){
